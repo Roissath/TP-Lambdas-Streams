1bb3b8331d0d35a6cd441186f3cf930ce68f0393
WABI ROISSATH <wabi.roissath@myges.fr>
2026-05-06 11:11:21 +0200

    Partie 1 - Filtrage (Exos 1-2-3) : longAndExpensiveTrips, badTrips, recentTrips

--- src/exo/Partie1.java
+++ src/exo/Partie1.java
@@ -2,22 +2,39 @@ package exo;
 
 import models.Trip;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Partie1 {
 
+    // Lambdas declarees en haut du fichier
+    Predicate<Trip> isLongTrip = trip -> trip.distanceKm() > 10;
+    Predicate<Trip> isExpensiveTrip = trip -> trip.price() > 20;
+    Predicate<Trip> isBadTrip = trip -> trip.rating() < 3;
+    Predicate<Trip> isRecentTrip = trip -> {
+        LocalDate today = LocalDate.now();
+        LocalDate yesterday = today.minusDays(1);
+        LocalDate tripDate = trip.startTime().toLocalDate();
+        return tripDate.equals(today) || tripDate.equals(yesterday);
+    };
+
     public List<Trip> longAndExpensiveTrips(List<Trip> trips) {
-        // distance > 10km et prix > 20
-        return List.of();
+        return trips.stream()
+                .filter(isLongTrip.and(isExpensiveTrip))
+                .collect(Collectors.toList());
     }
 
     public List<Trip> badTrips(List<Trip> trips) {
-        // rating < 3
-        return List.of();
+        return trips.stream()
+                .filter(isBadTrip)
+                .collect(Collectors.toList());
     }
 
     public List<Trip> recentTrips(List<Trip> trips) {
-        // aujourd hui ou hier
-        return List.of();
+        return trips.stream()
+                .filter(isRecentTrip)
+                .collect(Collectors.toList());
     }
 }
